aff3500b91cfbc1d96b059bed921e848143b5079
nirakar24 <jenashubham60@gmail.com>
2024-04-18 01:39:57 +0530

    new data

--- movies/models.py
+++ movies/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from datetime import *
 
 class Movie(models.Model):
     sequence_id = models.IntegerField(default=1)
